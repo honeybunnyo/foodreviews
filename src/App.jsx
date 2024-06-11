768541548034517f34fba427e3c41f77c714b774
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2024-06-11 17:14:19 +1000

    Added routes to recipes, restaurants and contacts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome/Welcome'
+import Recipes from './pages/Recipes/Recipes'
+import Restaurants from './pages/Restaurants/Restaurants'
+import Contacts from './pages/Contacts/Contacts'
+
 function App() {
 
   return (
     <> 
       <Routes>
         <Route path ='/' element = {<Welcome/>}/>
+        <Route path ='/Recipes' element = {<Recipes/>}/>
+        <Route path ='/Restaurants' element = {<Restaurants/>}/>
+        <Route path ='/Contact' element = {<Contacts/>}/>
       </Routes>
     </>
   )
